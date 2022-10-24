b12e96e40a25931e1401a94269ff0caa0a438ef6
Santi Villalon <villalonsantiago17@gmail.com>
2022-10-24 19:00:54 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+    alert("BIENVENIDOS");
+    let cliente = prompt ("Bueno dias ingrese su Apellido y Nombre.");
+    alert("Bienvenido" + cliente)
+    
+    let celulares = prompt ("Quiere comprar CELULARES. Ponga si o no");
+    if (celulares == "si"){
+        alert("Pefercto, ahora le vamos a mostrar los modelos que tenemos.")
+    }
+    
+    else if (celulares == "no"){
+        alert("No vamos a poder ayudarlo en esta ocacion, mucha suerte!!")
+    
+    
+    }
+     
